e7b3c65330159cc4d73303b70da1845627ecb3c8
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-28 22:14:58 +0200

    Added thumbnail support.

--- university-post-types.php
+++ university-post-types.php
@@ -46,7 +46,7 @@ function university_post_types()
             'singular_name' => "Professor"
         ),
         'menu_icon' => "dashicons-welcome-learn-more",
-        'supports' => array('title', 'editor'),
+        'supports' => array('title', 'editor', 'thumbnail'),
         'show_in_rest' => true,
     ));
 }
